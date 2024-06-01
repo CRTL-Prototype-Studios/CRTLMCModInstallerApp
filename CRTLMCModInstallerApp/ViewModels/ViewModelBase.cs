9a710ce03e042108cd47b692d21f91f5b6b9e482
Type-32 <87076491+Type-32@users.noreply.github.com>
2024-06-02 00:02:40 +0800

    Init Commit

---
+++ CRTLMCModInstallerApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace CRTLMCModInstallerApp.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ CRTLMCModInstallerApp/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+using SukiUI.Controls;
+
+namespace CRTLMCModInstallerApp.Views;
+
+public partial class MainWindow : SukiWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
