bc3ba915c2fcfeb4a416d6677fbb171ba43aa70e
Type-32 <87076491+Type-32@users.noreply.github.com>
2024-06-02 02:35:26 +0800

    Added New Modspace Menu
    Signed-off-by: Type-32 <87076491+Type-32@users.noreply.github.com>

---
+++ CRTLMCModInstallerApp/ViewModels/Startup/NewModspaceViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Avalonia.Collections;
+using Avalonia.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
+using ReactiveUI;
+
+namespace CRTLMCModInstallerApp.ViewModels.Startup;
+
+public partial class NewModspaceViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    private string _name = "", _description = "", _author = "", _minecraftVersion = "", _modloader = "fabric";
+
+    public AvaloniaList<string> ModloaderVersions { get; } = ["fabric", "forge", "neoforge", "quilt"];
+
+    public NewModspaceViewModel()
+    {
+        
+    }
+}
---
+++ CRTLMCModInstallerApp/Views/Startup/NewModspaceView.axaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CRTLMCModInstallerApp.Views.Startup;
+
+public partial class NewModspaceView : UserControl
+{
+    public NewModspaceView()
+    {
+        InitializeComponent();
+        
+    }
+}
