7e43eeb204b1375806fb7e0ab1afa2b006d6ca95
Type-32 <87076491+Type-32@users.noreply.github.com>
2024-06-03 00:27:52 +0800

    Idk
    Signed-off-by: Type-32 <87076491+Type-32@users.noreply.github.com>

---
+++ CRTLMCModInstallerApp/Core/Utility/FetchUtils.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Avalonia.Controls;
+
+namespace CRTLMCModInstallerApp.Core.Utility;
+
+public class FetchUtils
+{
+    public static async Task<string> OpenAndRetrieveDirectory(Window window)
+    {
+        OpenFolderDialog folderDialog = new OpenFolderDialog();
+        return await folderDialog.ShowAsync(window);
+    }
+}
